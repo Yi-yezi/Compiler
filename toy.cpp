9656e331bd5ffd3b001bcd7396af4944e80dd65a
Yi-yezi(laptop-win) <291067847@qq.com>
2020-01-03 18:04:16 +0800

    t3

--- toy.cpp
+++ toy.cpp
@@ -1,3 +1,13 @@
+
+#include <algorithm>
+#include <cctype>
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
 #include "llvm/ADT/APFloat.h"
 #include "llvm/ADT/STLExtras.h"
 #include "llvm/IR/BasicBlock.h"
@@ -9,14 +19,6 @@
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Type.h"
 #include "llvm/IR/Verifier.h"
-#include <algorithm>
-#include <cctype>
-#include <cstdio>
-#include <cstdlib>
-#include <map>
-#include <memory>
-#include <string>
-#include <vector>
 
 using namespace llvm;
 
